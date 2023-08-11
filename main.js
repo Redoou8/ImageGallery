f8c5a7f4a19bcc9496896b399fd0e4ab209e3490
Redoou8 <redouane3108@hotmail.com>
2023-08-11 18:49:33 +0200

    some change : next step nodeJS server

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel';
+
+// Initialisez le carrousel
+$('.owl-carousel').owlCarousel({
+  // Options du carrousel
+    items:1,
+
+});
+
+
+
+
+
+
+
 const images = document.querySelectorAll('.container img');
 
     images.forEach(image => {
@@ -17,3 +34,50 @@ const images = document.querySelectorAll('.container img');
             });
         });
     });
+
+
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const contentType = getContentType(filePath);
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    default:
+      return 'application/octet-stream';
+  }
+}
+
+
+
+
+
+
